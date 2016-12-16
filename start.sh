d289698feacedb87d83e17b1aafa63dbcb2d9bbe
Carlos Gonzalez <carlosj.gf@gmail.com>
2016-12-16 17:43:22 +0100

    Build

--- start.sh
+++ start.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 docker rm nginx > /dev/null 2>&1
-docker run -d --name nginx --net=host --restart unless-stopped \
+
+docker run \
+	-d \
+	--name nginx \
+	--network nginx \
+	--restart unless-stopped \
+	-p 80:80 \
+	-p 443:443 \
 	-v /:/hostfs:ro \
-	-v $DIR/logs:/var/log/nginx \
-	-v $DIR/conf.d:/etc/nginx/conf.d:ro \
+	-v nginx_conf:/etc/nginx/conf.d:ro \
 	-v $DIR/www:/etc/nginx/www:ro \
 	-v /etc/localtime:/etc/localtime:ro \
 	-v /etc/letsencrypt:/etc/letsencrypt:ro \
 	nginx:1.11.5-alpine
 
+#	-v nginx_logs:/var/log/nginx \
